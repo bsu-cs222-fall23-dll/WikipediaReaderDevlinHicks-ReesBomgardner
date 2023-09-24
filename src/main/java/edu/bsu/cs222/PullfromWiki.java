cb3ebac14319f786180248037653dd87a3d07600
Mythical-traveler <122392397+Mythical-traveler@users.noreply.github.com>
2023-09-24 12:53:45 -0400

    Version 0.1.3

--- src/main/java/edu/bsu/cs222/PullfromWiki.java
+++ src/main/java/edu/bsu/cs222/PullfromWiki.java
@@ -13,7 +13,7 @@ public class PullfromWiki {
         printRawJson(jsonData);
     }
 
-    private static URLConnection connectToWikipedia() throws IOException {
+    public static URLConnection connectToWikipedia() throws IOException {
         String encodedUrlString = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=" +
                 URLEncoder.encode("Zappa", Charset.defaultCharset()) +
                 "&rvprop=timestamp|user&rvlimit=4&redirects";
--- src/test/java/edu/bsu/cs222/tests.java
+++ src/test/java/edu/bsu/cs222/tests.java
@@ -1,5 +1,18 @@
 package edu.bsu.cs222;
 
+import java.io.IOException;
+import java.net.URLConnection;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class tests {
+    public void TestWikiConnection() throws IOException{
+        URLConnection connection=PullfromWiki.connectToWikipedia();
+        assertNotNull(connection);
+    }
+    public void TestJsonStream() throws IOException{
+
+    }
+
 
 }
